4db027aeb65ea297bca34651521b252dcf421f8f
Mark van Dam <114491487+Markusvdam@users.noreply.github.com>
2022-10-03 09:54:26 +0200

    JQuery Vegas (#7)
    JQuery Vegas (#7)

---
+++ js/image-information.js
@@ -0,0 +1,6 @@
+const mySlides = [
+  { src: "img/1-1900x1200.jpg", title: "Title #1", subtitle: "Subtitle #1"},
+  { src: "img/10-1900x1200.jpg", title: "Title #2", subtitle: "Subtitle #2"},
+  { src: "img/100-1900x1200.jpg", title: "Title #3", subtitle: "Subtitle #3"},
+  { src: "img/1000-1900x1200.jpg", title: "Title #4", subtitle: "Subtitle #4"},
+]
